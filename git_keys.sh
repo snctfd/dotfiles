298cc181eec0dbb6e48f027f6f4bfa73bad8fead
Rasmus Beck <rasmusbeck2k@outlook.com>
2022-12-23 00:57:38 +0100

    oopsie

--- git_keys.sh
+++ git_keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Generating git keys..."
-if [[ ! -e ~/.ssh/id_rsa ]] then
+if [[ ! -e ~/.ssh/id_ed25519 ]] then
     ssh-keygen -t ed25519 -C "evelyn@snctfd.io" -f ~/.ssh/id_ed25519
     ssh-add ~/.ssh/id_ed25519
 fi
