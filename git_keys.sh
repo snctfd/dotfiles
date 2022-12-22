b59e76da509b3639ba3747000d93f5aedafcfa04
Rasmus Beck <rasmusbeck2k@outlook.com>
2022-12-23 00:58:19 +0100

    make sure ssh-agent is running

--- git_keys.sh
+++ git_keys.sh
@@ -3,5 +3,6 @@
 echo "Generating git keys..."
 if [[ ! -e ~/.ssh/id_ed25519 ]] then
     ssh-keygen -t ed25519 -C "evelyn@snctfd.io" -f ~/.ssh/id_ed25519
+    eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519
 fi
