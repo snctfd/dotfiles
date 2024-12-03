6ca83bdfabb6bbd213fe268866c0a4299ddf19fc
Evelyn Beck <evelyn@snctfd.io>
2024-12-03 11:49:48 +0100

    Simplify apt install mechanism

--- install.sh
+++ install.sh
@@ -4,15 +4,8 @@
 sudo apt update
 sudo apt dist-upgrade
 
-function apt_install() {
-    if [[ ! -e "/usr/bin/$1" ]] then
-        sudo apt install -y $1
-    else
-        echo "$1 already installed -- skipping"
-    fi
-}
-
-apt_install zsh
+INSTALL_PACKAGES="zsh git"
+sudo apt install -y $INSTALL_PACKAGES
 
 ./setup-git.sh
 ./setup-zsh.sh
