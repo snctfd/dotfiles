ce712b5ca7c3dc41edc2beb7197c56dc55b223b2
Evelyn Beck <evelyn@snctfd.io>
2024-12-04 13:35:57 +0100

    Change setup-git.sh to prompt for name and email

--- setup-git.sh
+++ setup-git.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "Setting up SSH key and Git config..."
+
+read -p "Enter e-mail to use for key generation and Git config" EMAIL
+read -p "Enter full name to use for Git config" FULLNAME
+
 if [[ ! -e ~/.ssh/id_ed25519 ]] then
     echo "Generating SSH key..."
-    ssh-keygen -t ed25519 -C "evelyn@snctfd.io" -f ~/.ssh/id_ed25519
+    ssh-keygen -t ed25519 -C "$EMAIL" -f ~/.ssh/id_ed25519
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519
 else 
     echo "Skipping generation of SSH key!"
 fi
 
-git config --global user.email "evelyn@snctfd.io"
-git config --global user.name "Evelyn Beck"
+git config --global user.email "$EMAIL"
+git config --global user.name "$FULLNAME"
